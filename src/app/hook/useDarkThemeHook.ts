5cbbade383d5cce03d777d940aeed73529c78c9b
pioneer2077 <134494401+pioneer2077@users.noreply.github.com>
2024-01-05 21:05:18 +0900

    darkmode

--- src/app/hook/useDarkThemeHook.ts
+++ src/app/hook/useDarkThemeHook.ts
@@ -1,30 +1,30 @@
 import { useEffect, useState } from "react";
 
-// const useLocalStorage = (key, initialValue) => {
-//   const [storedValue, setStoredValue] = useState(() => {
-//     try {
-//       const item = window.localStorage.getItem(key);
-//       return item ? JSON.parse(item) : initialValue;
-//     } catch (error) {
-//       console.log(error);
-//       return initialValue;
-//     }
-//   });
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
 
-//   const setValue = (value) => {
-//     try {
-//       const valueToStore =
-//         value instanceof Function ? value(storedValue) : value;
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
 
-//       setStoredValue(valueToStore);
+      setStoredValue(valueToStore);
 
-//       window.localStorage.setItem(key, JSON.stringify(valueToStore));
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-//   return [storedValue, setValue];
-// };
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return [storedValue, setValue];
+};
 
 const useDarkMode = () => {
   // const [enabled, setEnabled] = useLocalStorage("dark-theme");
